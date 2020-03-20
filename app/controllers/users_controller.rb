4ae325243bd5357bc01111090412b38725b08f30
Angel Cordova <ae_cordova@outlook.com>
2020-03-20 11:01:04 -0600

    Added User's Signup form and simple Show view, along with controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new;  end
-  def create;  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new (user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
+
