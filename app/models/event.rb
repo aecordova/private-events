836bcdec473a5229c4e8b0311ef1aa2a2b56a11d
Angel Cordova <ae_cordova@outlook.com>
2020-03-21 15:11:08 -0600

    Added scopes for past and upcoming events, also extracted events section to partial, for reuse in several places

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ApplicationRecord
   belongs_to :organizer, class_name: 'User'
   has_many :event_attendances
   has_many :attendees, through: 'event_attendances', source: 'user'
-  
+  scope :past, -> { where("date < ?", Date.today) }
+  scope :upcoming, -> { where("date >= ?", Date.today) }
 end
